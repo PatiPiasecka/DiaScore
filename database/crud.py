68c0dc6618ca5f858e9df8490965e0154190bb56
Patrycja Zborowska <zborowskapati@gmail.com>
2026-04-27 20:47:29 +0200

    Changed dict to model_dump (dict is deprecated), added try except block to handle errors

--- database/crud.py
+++ database/crud.py
@@ -10,11 +10,15 @@ def get_records(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.DiabetesRecord).offset(skip).limit(limit).all()
 
 def create_diabetes_record(db: Session, record: schemas.DiabetesCreate):
-    db_record = models.DiabetesRecord(**record.dict())
-    db.add(db_record)
-    db.commit()
-    db.refresh(db_record)
-    return db_record
+    db_record = models.DiabetesRecord(**record.model_dump())
+    try:
+        db.add(db_record)
+        db.commit()
+        db.refresh(db_record)
+        return db_record
+    except Exception as e:
+        db.rollback()  # Rollback in case of an error
+        raise e
 
 def get_patient_count(db: Session):
     return db.query(models.DiabetesRecord).count()
