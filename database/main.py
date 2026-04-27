1da75206946ac28b350caf766f47a76b61b05b0d
Patrycja Zborowska <zborowskapati@gmail.com>
2026-04-27 20:51:31 +0200

    Changed records insert. Used bulk_insert_mappings instead of add_all() for much faster inserts and lower memory usage (avoids ORM overhead and per-object tracking).

--- database/main.py
+++ database/main.py
@@ -12,24 +12,27 @@ def import_csv_to_db(csv_file_path: str):
     
     try:
         print(f"Starting the import for {len(df)} records...")
-        
-        for _, row in df.iterrows():
-            db_record = models.DiabetesRecord(
-                pregnancies=int(row['Pregnancies']),
-                glucose=int(row['Glucose']),
-                blood_pressure=int(row['BloodPressure']),
-                skin_thickness=int(row['SkinThickness']),
-                insulin=int(row['Insulin']),
-                bmi=float(row['BMI']),
-                diabetes_pedigree_function=float(row['DiabetesPedigreeFunction']),
-                age=int(row['Age']),
-                outcome=int(row['Outcome'])
-            )
-            db.add(db_record)
-        
+
+        records = df.to_dict(orient="records")
+
+        db.bulk_insert_mappings(models.DiabetesRecord, [
+            {
+                "pregnancies": int(row['Pregnancies']),
+                "glucose": int(row['Glucose']),
+                "blood_pressure": int(row['BloodPressure']),
+                "skin_thickness": int(row['SkinThickness']),
+                "insulin": int(row['Insulin']),
+                "bmi": float(row['BMI']),
+                "diabetes_pedigree_function": float(row['DiabetesPedigreeFunction']),
+                "age": int(row['Age']),
+                "outcome": int(row['Outcome'])
+            }
+            for row in records
+        ])
+
         db.commit()
-        print("Import was succesful")
-        
+        print("Import was successful")
+
     except Exception as e:
         print(f"There was an error during import: {e}")
         db.rollback()
