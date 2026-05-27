f1192fcd1c34b07236d6fed82f78fc9547ead492
Patrycja Zborowska <zborowskapati@gmail.com>
2026-05-27 22:07:47 +0200

    chore(test_imputation): add dpf imputation in tests

--- api/api_test/test_imputation.py
+++ api/api_test/test_imputation.py
@@ -21,3 +21,6 @@ def test_predict_endpoint_applies_imputation(client):
     assert created["skin_thickness"] != 0, "skin_thickness should be imputed (not 0)"
     assert created["insulin"] != 0, "insulin should be imputed (not 0)"
     assert created["bmi"] != 0.0, "bmi should be imputed (not 0.0)"
+    
+    assert "diabetes_pedigree_function" in created
+    assert created["diabetes_pedigree_function"] > 0, "DPF should be automatically calculated"
